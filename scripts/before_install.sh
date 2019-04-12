49abebc817ef1f0d4dca49fbbb2df9653b1d0b06
yakult1995 <destin.tax@outlook.jp>
2019-04-12 00:56:01 -0700

    :zzz:

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-tree
-
 docker-compose -f /home/ec2-user/go-posgre-restapi/docker-compose.yml down > /dev/null 2>&1
 
 mv /home/ec2-user/go-posgre-restapi/docker/go/.env /tmp/go.env > /dev/null 2>&1
